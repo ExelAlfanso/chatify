4adcb6dee91125191447f2bb2ac3f800d8da51eb
Exel Boy Alfanso <72937148+ExelAlfanso@users.noreply.github.com>
2025-08-08 16:03:54 +0700

    fix: id fix

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import React from "react";
-import Hero from "@/sections/Hero";
+import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 
 interface HomeProps {
-  id: string;
+  id?: string;
   className?: string;
   children?: React.ReactNode;
 }
