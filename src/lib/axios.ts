f317671c85218456c590289bc9ed9b8cfdc931e0
Exel Boy Alfanso <72937148+ExelAlfanso@users.noreply.github.com>
2025-08-08 16:42:47 +0700

    fix: credentials bug (?)

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "https://mywebsocketapp.onrender.com/api",
-  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
