c2cf2ee51b9093439faa3919684f2d1acee1c9b4
Exel Boy Alfanso <72937148+ExelAlfanso@users.noreply.github.com>
2025-08-18 20:16:12 +0700

    fix: baseUrl dynamic url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,12 +1,7 @@
 import axios from "axios";
-import dotenv from "dotenv";
-dotenv.config();
 
 const axiosInstance = axios.create({
-  baseURL:
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3001/api"
-      : "https://mywebsocketapp.onrender.com/api",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
